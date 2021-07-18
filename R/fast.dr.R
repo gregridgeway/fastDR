b128c733b231342d32ca7ca536e1247207dd56e9
Greg Ridgeway <gregridgeway@gmail.com>
2021-07-18 15:31:48 -0400

    Update warning about needing a patched version of survey 4.0

--- R/fast.dr.R
+++ R/fast.dr.R
@@ -8,8 +8,7 @@
    vers <- vers[grep("Version:",vers)]
    vers <- rev(strsplit(vers," ")[[1]])[1]
    packageStartupMessage(paste("Loaded fastDR",vers))
-   if(!("rescale" %in% names(formals(survey:::svyglm))))
-      packageStartupMessage('WARNING: fastDR requires a modified version of the survey package. The R survey package normalizes the weights in a particular way (normalizes across the whole dataset) that can cause numerical instability. fastDR() normalizes within the treatment and control groups so that the largest weight within each is 1. Run remotes::install_github("gregridgeway/fastDR") to obtain a patched version of the survey package that avoids the weight rescaling.')
+   packageStartupMessage('WARNING: fastDR requires a modified version of the survey package. survey 4.0 has a bug when rescale=FALSE that does not copy the weights into the dataset. Run remotes::install_github("gregridgeway/survey","patch-2") to obtain a patched version of the survey package that corrects this.')
 }
 
 make.fastDR.formula <-
